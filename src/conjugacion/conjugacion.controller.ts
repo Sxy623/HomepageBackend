55d922e1875597040512744dfbba79ad19cc2c08
Sxy623 <191436989@qq.com>
2021-01-30 22:09:24 +0800

    Init and get random question

--- src/conjugacion/conjugacion.controller.ts
+++ src/conjugacion/conjugacion.controller.ts
@@ -1,12 +1,51 @@
 import { Controller, Get } from '@nestjs/common';
 import { ConjugacionService } from './conjugacion.service';
+import { Question } from '../entities/question.entity';
 
 @Controller('conjugacion')
 export class ConjugacionController {
   constructor(private readonly conjugacionService: ConjugacionService) {}
 
+  @Get('init-question')
+  async initQuestion(): Promise<void> {
+    const questions = [
+      {
+        description: 'ser',
+        answers: ['soy', 'eres', 'es', 'somos', 'sois', 'son'],
+      },
+      {
+        description: 'hablar',
+        answers: ['hablo', 'hablas', 'habla', 'hablamos', 'habláis', 'hablan'],
+      },
+      {
+        description: 'comer',
+        answers: ['como', 'comes', 'come', 'comemos', 'coméis', 'comen'],
+      },
+      {
+        description: 'escribir',
+        answers: [
+          'escribo',
+          'escribes',
+          'escribe',
+          'escribimos',
+          'escribís',
+          'escriben',
+        ],
+      },
+    ];
+
+    await this.conjugacionService.clear();
+
+    for (const question of questions) {
+      const newQuestion = new Question();
+      newQuestion.description = question.description;
+      newQuestion.answers = JSON.stringify(question.answers);
+      await this.conjugacionService.save(newQuestion);
+    }
+  }
+
   @Get('get-question')
-  getQuestion(): string {
-    return this.conjugacionService.getQuestion();
+  async getQuestion(): Promise<Question> {
+    return this.conjugacionService.getRandomOne();
   }
 }
--- src/conjugacion/conjugacion.service.ts
+++ src/conjugacion/conjugacion.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Question } from '../entities/question.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ConjugacionService {
-  getQuestion(): string {
-    return 'hablar';
+  constructor(
+    @InjectRepository(Question)
+    private readonly questionRepository: Repository<Question>,
+  ) {}
+
+  async clear(): Promise<void> {
+    await this.questionRepository.clear();
+  }
+
+  async save(question: Question): Promise<Question> {
+    return await this.questionRepository.save(question);
+  }
+
+  async getRandomOne(): Promise<Question> {
+    const count = await this.questionRepository.count();
+    const id = Math.floor(Math.random() * count) + 1;
+    return await this.questionRepository.findOne(id);
   }
 }
