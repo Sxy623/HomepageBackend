d2ab61d2808879bdfb755542202a026a9c1305f2
Sxy623 <191436989@qq.com>
2021-01-30 19:37:10 +0800

    Connect database and add entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConjugacionController } from './conjugacion/conjugacion.controller';
 import { ConjugacionService } from './conjugacion/conjugacion.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '123456',
+      database: 'homepage',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController, ConjugacionController],
   providers: [AppService, ConjugacionService],
 })
---
+++ src/entities/question.entity.ts
@@ -0,0 +1,13 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Question {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  description: string;
+
+  @Column()
+  answers: string;
+}
